9656e93e45e80a56996ffeb9e4001186069145da
isaiah francois <francoisisaiaht@yahoo.com>
2020-08-12 23:09:42 -0400

    Request video/audio permissions from users and displayed live video

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.status(200).send(`Server is listening on port 5000`);
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 server.listen(5000);
